546df5440203e3a8e8113170b4196b23a3a66c3b
alamalcantara <alamalcantara99@gmail.com>
2020-07-17 21:13:20 -0400

    fix: adding condition for dotenv

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
-const dotenv = require('dotenv');
-dotenv.config();
+if(process.env.NODE_ENV === 'development') {
+    const dotenv = require('dotenv');
+    dotenv.config();
+}
 
 module.exports = {
     NODE_ENV: process.env.NODE_ENV,
